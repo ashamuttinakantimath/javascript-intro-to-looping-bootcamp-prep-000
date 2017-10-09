9803dbfe072cfd3f19a4e4317021c10784b18e9e
asha muttinakntimath <asha.mumath@gmail.com>
2017-10-09 02:02:48 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,14 @@
+var gifts = ['teddy bear', 'drone', 'doll']
+
+function forloop(gifts) {
+  for(var i=0;i<25;i++)
+  console.log(`Wrapped ${gifts} and added a bow!`)
+}
+
+let no=100
+function whileLoop(no)
+{
+  return no
+}
+while(no>0)
+console.log(--no)
